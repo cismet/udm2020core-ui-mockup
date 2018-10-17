4a4077c82b24452e02ad4ef6fbe451fdf3637997
helllth <th@cismet.de>
2018-10-17 18:43:19 +0200

    next step towards a mockup for v2: scrolling in results

--- src/views/udm_Ergebnisse/Results.js
+++ src/views/udm_Ergebnisse/Results.js
@@ -11,13 +11,15 @@ import {
   Collapse,
   Fade
 } from 'reactstrap';
-import {AppSwitch} from '@coreui/react'
+import {Link} from "react-scroll";
 
 import {Map, TileLayer, ZoomControl} from "react-leaflet";
 import usersData from './ResultsData'
 import "leaflet/dist/leaflet.css"
 import {FullscreenControl, LocateControl} from "react-cismap";
+import Scroll from "react-scroll"
 
+var scroll = Scroll.animateScroll;
 function ResultRow(props) {
   const resRow = props.user
 
@@ -63,7 +65,13 @@ function ResultRow(props) {
 
       </td>
       <td>
-        <Button block outline color="primary">
+        <Button
+          onClick={() => {
+          scroll.scrollToBottom()
+        }}
+          block
+          outline
+          color="primary">
           <i className="fa fa-plus-square"/></Button>
       </td>
 
@@ -72,6 +80,7 @@ function ResultRow(props) {
 }
 
 class Users extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -93,28 +102,36 @@ class Users extends Component {
     const position = [48.210033, 16.363449];
     const userList = usersData.filter((user) => user.id < 10)
     console.log(FullscreenControl)
-    return (
-      <div className="animated fadeIn">
-        <Card>
-          <CardHeader>
-            <i className="fa fa-align-justify"></i>
-            Ergebnisse
-            <small className="text-muted">{"  "}(5 Themen, 105 Schadstoffe, aktueller Kartenausschnitt)</small>
-            <div className="card-header-actions">
-              <a
-                className="card-header-action btn btn-minimize"
-                data-target="#collapseExample"
-                onClick={this.toggle}>
-                <i className="fa fa-table"></i>
-              </a>
-              <a
-                className="card-header-action btn btn-minimize"
-                title=""
-                data-target="#collapseExample"
-                onClick={this.toggle}>
-                <i className="fa fa-map"></i>
-              </a>
 
+    return (
+      <div className="animated fadeIn" id="resultsDIV">
+        <Row>
+          <Col>
+            <Fade timeout={this.state.timeout} in={this.state.fadeIn}>
+              <Card>
+                <CardHeader>
+                  <i className="fa fa-align-justify"></i>
+                  Ergebnisstabelle
+                  <small className="text-muted">{"  "}(5 Themen, 105 Schadstoffe, aktueller Kartenausschnitt)</small>
+                  <div className="card-header-actions">
+                    <a
+                      className="card-header-action btn btn-minimize"
+                      data-target="#collapseExample"
+                      onClick={() => {
+                      scroll.scrollToTop();
+                    }}>
+                      <i className="fa fa-table"></i>
+                    </a>
+                    <a
+                      className="card-header-action btn btn-minimize"
+                      data-target="#collapseExample"
+                      onClick={() => {
+                      scroll.scrollToBottom();
+                    }}>
+                      <i className="fa fa-map"></i>
+                    </a>
+
+                    {/*
               <a
                 className="card-header-action btn btn-minimize"
                 data-target="#collapseExample"
@@ -123,15 +140,9 @@ class Users extends Component {
                 marginLeft: "25px"
               }}>
                 <i className="fa fa-exchange"></i>
-              </a>
-            </div>
-          </CardHeader>
-        </Card>
-
-        <Row>
-          <Col>
-            <Fade timeout={this.state.timeout} in={this.state.fadeIn}>
-              <Card>
+              </a> */}
+                  </div>
+                </CardHeader>
                 <CardBody>
                   <Collapse isOpen={this.state.collapse} id="collapseExample">
 
@@ -160,44 +171,80 @@ class Users extends Component {
         </Row>
         <Row>
           <Col>
-            <Card>
-              <CardBody>
-                <Map
-                  style={{
-                  height: 500
-                }}
-                  center={position}
-                  zoom={10}
-                  attributionControl={false}
-                  zoomControl={false}>
-                  <TileLayer
-                    noattribution='&copy; <a href="http://basemap.at">Basemap.at</a>'
-                    subdomains={['maps', 'maps1', 'maps2', 'maps3', 'maps4']}
-                    url='https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png'
-                    opacity={0.5}
-                    cssFilter='filter:grayscale(0.9)brightness(0.9)invert(1);'/>
-                  <ZoomControl
-                    position="topleft"
-                    zoomInTitle="Vergr&ouml;ßern"
-                    zoomOutTitle="Verkleinern"/>
-                  <FullscreenControl
-                    title="Vollbildmodus"
-                    forceSeparateButton={true}
-                    titleCancel="Vollbildmodus beenden"
-                    position="topleft"/>
-                  <LocateControl
-                    setView="once"
-                    flyTo={true}
-                    strings={{
-                    title: "Mein Standort",
-                    metersUnit: "Metern",
-                    feetUnit: "Feet",
-                    popup: "Sie befinden sich im Umkreis von {distance} {unit} um diesen Punkt.",
-                    outsideMapBoundsMsg: "Sie gefinden sich wahrscheinlich außerhalb der Kartengrenzen."
-                  }}/>
-                </Map>
-              </CardBody>
-            </Card>
+            <div name="resultsinmap">
+              <Card >
+                <CardBody>
+                  <Map
+                    style={{
+                    height: 500
+                  }}
+                    center={position}
+                    zoom={10}
+                    attributionControl={false}
+                    zoomControl={false}>
+                    <TileLayer
+                      noattribution='&copy; <a href="http://basemap.at">Basemap.at</a>'
+                      subdomains={['maps', 'maps1', 'maps2', 'maps3', 'maps4']}
+                      url='https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png'
+                      opacity={0.5}
+                      cssFilter='filter:grayscale(0.9)brightness(0.9)invert(1);'/>
+                    <ZoomControl
+                      position="topleft"
+                      zoomInTitle="Vergr&ouml;ßern"
+                      zoomOutTitle="Verkleinern"/>
+                    <FullscreenControl
+                      title="Vollbildmodus"
+                      forceSeparateButton={true}
+                      titleCancel="Vollbildmodus beenden"
+                      position="topleft"/>
+                    <LocateControl
+                      setView="once"
+                      flyTo={true}
+                      strings={{
+                      title: "Mein Standort",
+                      metersUnit: "Metern",
+                      feetUnit: "Feet",
+                      popup: "Sie befinden sich im Umkreis von {distance} {unit} um diesen Punkt.",
+                      outsideMapBoundsMsg: "Sie gefinden sich wahrscheinlich außerhalb der Kartengrenzen."
+                    }}/>
+                  </Map>
+                </CardBody>
+                <CardHeader>
+                  <i className="fa fa-align-justify"></i>
+                  Ergebnisse in der Karte
+                  <small className="text-muted">{"  "}(5 Themen, 105 Schadstoffe, aktueller Kartenausschnitt)</small>
+                  <div className="card-header-actions">
+                    <a
+                      className="card-header-action btn btn-minimize"
+                      data-target="#collapseExample"
+                      onClick={() => {
+                      scroll.scrollToTop();
+                    }}>
+                      <i className="fa fa-table"></i>
+                    </a>
+                    <a
+                      className="card-header-action btn btn-minimize"
+                      data-target="#collapseExample"
+                      onClick={() => {
+                      scroll.scrollToBottom();
+                    }}>
+                      <i className="fa fa-map"></i>
+                    </a>
+
+                    {/*
+              <a
+                className="card-header-action btn btn-minimize"
+                data-target="#collapseExample"
+                onClick={this.toggle}
+                style={{
+                marginLeft: "25px"
+              }}>
+                <i className="fa fa-exchange"></i>
+              </a> */}
+                  </div>
+                </CardHeader>
+              </Card>
+            </div>
           </Col>
         </Row>
       </div>
